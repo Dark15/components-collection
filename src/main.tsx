c1d9f642c13b05d8b8781ad309c3057f863b11ac
Starry <starrycat15@gmail.com>
2021-11-10 13:28:47 +0800

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import {StrictMode} from 'react'
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+)
